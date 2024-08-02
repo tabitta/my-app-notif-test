513a09188f1fde78b341021aff3045b2ac30edf0
tabitta <tabita.ctin@gmail.com>
2024-08-02 12:35:26 +0300

    Install expo-dev-client and expo-notifications

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,4 +1,6 @@
-import { Image, StyleSheet, Platform } from 'react-native';
+import * as Notifications from 'expo-notifications';
+
+import { Image, StyleSheet, Platform, Pressable, Text } from 'react-native';
 
 import { HelloWave } from '@/components/HelloWave';
 import ParallaxScrollView from '@/components/ParallaxScrollView';
@@ -6,6 +8,31 @@ import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
 
 export default function HomeScreen() {
+
+  const scheduleNotifications  = async () => {
+    //cancel previous notifications
+    await Notifications.cancelAllScheduledNotificationsAsync();
+
+    //schedule new notifications
+    await Notifications.scheduleNotificationAsync({
+      content: {
+        title: "new notification title",
+      },
+      trigger: {
+        hour: 8,
+        minute: 0,
+        repeats: true
+      }
+    });
+    console.log('Notifications scheduled');
+  }
+
+  const getNotifications  = async () => {
+    console.log('Get schedules notifications');
+    const enabledNotifications = await Notifications.getAllScheduledNotificationsAsync();
+    console.log(enabledNotifications);
+  }
+
   return (
     <ParallaxScrollView
       headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
@@ -19,6 +46,19 @@ export default function HomeScreen() {
         <ThemedText type="title">Welcome!</ThemedText>
         <HelloWave />
       </ThemedView>
+
+      <ThemedView>
+          <Pressable onPress={() => scheduleNotifications()}>
+            <Text>Schedule Notifications</Text>
+          </Pressable>
+      </ThemedView>
+
+      <ThemedView>
+        <Pressable onPress={() => getNotifications()}>
+            <Text>Get Notifications</Text>
+          </Pressable>
+      </ThemedView>
+
       <ThemedView style={styles.stepContainer}>
         <ThemedText type="subtitle">Step 1: Try it</ThemedText>
         <ThemedText>
